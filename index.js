7b92d255f26fec0ef2bc0da832331a62c20852ce
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 02:39:40 +0300

    add methods that use instance data and parameter data

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+class Bird {
+    constructor(name, gender) {
+        this.name = name
+        this.gender = gender
+    }
+    speak() {
+        return `${this.name} says squawk!`
+    }
+}
+class Cat extends Bird {
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+class Dog extends Bird {
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
